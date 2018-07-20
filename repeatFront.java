ff6fff51ef3876676b98bcdf91f1a413f3990fbb
kranthibasaraju <37279088+kranthibasaraju@users.noreply.github.com>
2018-07-20 12:57:04 -0400

    Create repeatFront.java

---
+++ repeatFront.java
@@ -0,0 +1,17 @@
+import java.io.StringReader;
+
+public class repeatFront {
+	public static String repeatFront(String s,int n)
+	{
+		String repeat = s.substring(0, n);
+		String result="";
+		result=result.concat(repeat);
+		while(true) {
+			if(n==0)
+				break;
+			result=result.concat(result.substring(0,--n));
+		}
+		return result;
+	}
+
+}
