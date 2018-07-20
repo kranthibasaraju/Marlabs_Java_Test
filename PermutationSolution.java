00a8a5678ee93ae46d3e423499e0221cb5f10561
kranthibasaraju <37279088+kranthibasaraju@users.noreply.github.com>
2018-07-20 13:51:35 -0400

    Create PermutationSolution.java

---
+++ PermutationSolution.java
@@ -0,0 +1,14 @@
+public class PermutationSolution {
+
+	
+	public static int PermutationSolution(int a[]) {
+		int natural=a.length+1;
+		int b=1;
+		for(int i:a)
+			if(i>=natural)
+				b=0;
+		
+		return b;
+	}
+
+}
